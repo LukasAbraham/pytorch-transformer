1c7103de37b7252a09b0897c9cdda2e512d157b2
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-06-15 17:17:06 +0700

    add log in train.py

--- train.py
+++ train.py
@@ -130,6 +130,7 @@ def get_all_sentences(ds, lang):
 
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    print(tokenizer_path)
     if not Path.exists(tokenizer_path):
         # Most code taken from: https://huggingface.co/docs/tokenizers/quicktour
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
